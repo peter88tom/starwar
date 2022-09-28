c6c04b0ed20ffd4699251151cd7fd568b3ec52d0
petro_thomas <peter88tom@gmail.com>
2022-09-28 15:45:54 +0200

    Make api endpoint as default url

--- admin/urls.py
+++ admin/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from graphene_django.views import GraphQLView
 from api.schema import schema
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+    path('', include('api.urls')),
+    #path('graphql', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
 ]
---
+++ api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from graphene_django.views import GraphQLView
+from api.schema import schema
+from django.views.decorators.csrf import csrf_exempt
+
+urlpatterns = [
+    path('', csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
+]
